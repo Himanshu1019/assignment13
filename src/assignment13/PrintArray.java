20734debd3cd5f919a07cc39ac8f1e50df50d577
classroom <himanshukanoje23@gmail.com>
2022-04-10 16:21:49 +0530

    my commit

--- src/assignment13/PrintArray.java
+++ src/assignment13/PrintArray.java
@@ -23,6 +23,25 @@ public class PrintArray {
 				
 			
 		}
+	public static void toprint(Float[] arr)
+	{
+		
+		
+			if(arr[0] >arr[1] && arr[0] >arr[2])
+			{
+				System.out.println("biggest of three number is "+arr[0]);
+				}
+			else if(arr[1] >arr[0] && arr[1] >arr[2])
+			{
+				System.out.println("biggest of three number is "+arr[1]);	
+			}
+			else
+			{
+				System.out.println("biggest of three number is "+arr[2]);	
+			}
+				
+			
+		}
 	
 	
 	
@@ -31,12 +50,11 @@ public class PrintArray {
 		Integer[] intarray= {5,8,4};
 		Float[] floatarray= {1.3f,1.6f,1.62f};
 		
-		//String[] stringarray= {"hima","aayu"};
-		//Double[] doublearray= {1.1,1.2,1.3,1.4};
+		
 		PrintArray.toprint(intarray);
 		
-		//PrintArray.toprint(stringarray);
-		//PrintArray.toprint(doublearray);
+		
+		PrintArray.toprint(floatarray);
 		
 		
 
