c037802b0be7567352b493224596a54d8a17c573
classroom <himanshukanoje23@gmail.com>
2022-04-10 16:07:48 +0530

    my commit

--- src/assignment13/PrintArray.java
+++ src/assignment13/PrintArray.java
@@ -1,24 +1,42 @@
 package assignment13;
 
+import java.util.Scanner;
+
 public class PrintArray {
 	
-	public static <E> void toprint(E[] intarr)
+	public static void toprint(Integer[] arr)
 	{
-		for(E element : intarr)
-		{
-			System.out.println(element);
-		}System.out.println(".....");
-	}
+		
+		
+			if(arr[0] >arr[1] && arr[0] >arr[2])
+			{
+				System.out.println("biggest of three number is "+arr[0]);
+				}
+			else if(arr[1] >arr[0] && arr[1] >arr[2])
+			{
+				System.out.println("biggest of three number is "+arr[1]);	
+			}
+			else
+			{
+				System.out.println("biggest of three number is "+arr[2]);	
+			}
+				
+			
+		}
+	
 	
 	
 	public static void main(String[] args)
 {
-		Integer[] intarray= {1,2,3,4,5,6};
-		String[] stringarray= {"hima","aayu"};
-		Double[] doublearray= {1.1,1.2,1.3,1.4};
+		Integer[] intarray= {5,8,4};
+		Float[] floatarray= {1.3f,1.6f,1.62f};
+		
+		//String[] stringarray= {"hima","aayu"};
+		//Double[] doublearray= {1.1,1.2,1.3,1.4};
 		PrintArray.toprint(intarray);
-		PrintArray.toprint(stringarray);
-		PrintArray.toprint(doublearray);
+		
+		//PrintArray.toprint(stringarray);
+		//PrintArray.toprint(doublearray);
 		
 		
 
